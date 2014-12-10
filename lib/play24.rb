f26def86333a8eadf2c65fb4be5b5ccbc95c8c4b
Lucjan Suski <lucjansuski@gmail.com>
2014-12-10 01:35:27 +0100

    remove redis-semaphore

--- lib/play24.rb
+++ lib/play24.rb
@@ -2,7 +2,6 @@ require "mechanize"
 require "play24/version"
 require "play24/client"
 require "play24/procedures/login_procedure"
-require "redis-semaphore"
 
 module Play24
   # Your code goes here...
